d921ceeac11203187818cd8912323be7e8483bb1
evseen1337-dot <evseen1337@gmail.com>
2026-03-02 17:24:32 +0500

    app.py

--- app.py
+++ app.py
@@ -58,6 +58,6 @@ async def grab():
         await client.disconnect()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
